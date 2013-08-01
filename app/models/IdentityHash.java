b8ee24a86fd2cc2e969298d12d9d726ccd9786a6
Martin Page <wookoouk@gmail.com>
2013-08-01 12:34:09 +0100

    New Hash method

--- app/models/IdentityHash.java
+++ app/models/IdentityHash.java
@@ -1,6 +1,8 @@
 package models;
 
+import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Random;
 
 import javax.persistence.Entity;
@@ -16,16 +18,21 @@ public class IdentityHash extends Model {
 	 * 
 	 */
 	private static final long serialVersionUID = 965039885280462355L;
-	private static final String saltPrefix = "deadsea";
 	private String salt;
 	private String digest;
 
 	// DONE add a dash of salt
 
 	public IdentityHash(String identity) {
-		salt = saltPrefix + generateSalt();
-		Sha256Crypt.Sha256_crypt(identity, salt, 0);
-//		digest = DigestUtils.shaHex(identity + salt);
+		salt = generateSalt();
+
+		String saltPlusPlainTextPassword = salt + new String(identity);
+		try {
+			digest = hash256(saltPlusPlainTextPassword);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		System.out.println("DIGEST: " + digest);
 	}
 
 	public String getDigest() {
@@ -36,9 +43,26 @@ public class IdentityHash extends Model {
 		return salt;
 	}
 
+	public static String hash256(String data) throws NoSuchAlgorithmException {
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+		try {
+			md.update(data.getBytes("UTF-8"));
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		return bytesToHex(md.digest());
+	}
+
+	public static String bytesToHex(byte[] bytes) {
+		StringBuffer result = new StringBuffer();
+		for (byte byt : bytes)
+			result.append(Integer.toString((byt & 0xff) + 0x100, 16).substring(
+					1));
+		return result.toString();
+	}
+
 	private static String generateSalt() {
 
-		
 		char[] chars = "abcdefghijklmnopqrstuvwxyz".toCharArray();
 		StringBuilder sb = new StringBuilder();
 		Random random = new Random();
