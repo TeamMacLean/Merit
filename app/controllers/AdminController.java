6a37b30d1f2b2ca268b383237c42de78b8137ddd
Martin Page <wookoouk@gmail.com>
2013-08-20 16:05:57 +0100

    templated emails

--- app/controllers/AdminController.java
+++ app/controllers/AdminController.java
@@ -20,7 +20,7 @@ public class AdminController extends Controller {
 
 	public static Result userAdmin() {
 
-       Form<User> userForm = new Form<User>(User.class);
+		Form<User> userForm = new Form<User>(User.class);
 		List<User> users = User.findAll();
 		return ok(useradmin.render(users, userForm));
 		// return TODO;
@@ -30,31 +30,44 @@ public class AdminController extends Controller {
 		Form<User> userForm = new Form<User>(User.class).bindFromRequest();
 		if (userForm.hasErrors()) {
 			List<User> users = User.findAll();
-            flash(Application.GLOBAL_FLASH_ERROR,"ERROR!");
+			flash(Application.GLOBAL_FLASH_ERROR, "An unknown error occured.");
 			return badRequest(useradmin.render(users, userForm));
 			// return TODO;
 		}
+		User existingUser = User.findByEmail(userForm.get().email);
+		if (existingUser != null) {
+			flash(Application.GLOBAL_FLASH_ERROR,
+					"A user with the email address \"userForm.get().email\" already exists.");
+			List<User> users = User.findAll();
+			return badRequest(useradmin.render(users, userForm));
+		}
+
 		User newUser = new User(userForm.get().name, userForm.get().email);
 		newUser.save();
-		if(!EmailController.NotifyNewUser(newUser, request())){
-            flash(Application.GLOBAL_FLASH_ERROR,"Cound not notify user, please check your email settings");
-        } else {
-            flash(Application.GLOBAL_FLASH_SUCCESS, "User created and email sent");
-        }
+		if (!EmailController.NotifyNewUser(newUser, request())) {
+			flash(Application.GLOBAL_FLASH_ERROR,
+					"Cound not notify user, please check your email settings");
+			newUser.delete();
+		} else {
+			flash(Application.GLOBAL_FLASH_SUCCESS,
+					"User created and email sent");
+		}
 
 		return redirect(routes.AdminController.userAdmin());
 	}
 
 	public static Result deleteUser(Long id) {
-        int userCount = User.findAll().size();
-        if(userCount == 1){
-            flash(Application.GLOBAL_FLASH_ERROR,"You are the last user, You cant delete yourself");
-//            return badRequest("You are the last user, You cant delete yourself");
-        } else {
-            User user = User.findById(id);
-            user.delete();
-        }
-        return redirect(routes.AdminController.userAdmin());
+		int userCount = User.findAll().size();
+		if (userCount == 1) {
+			flash(Application.GLOBAL_FLASH_ERROR,
+					"You are the last user, You cant delete yourself");
+			// return
+			// badRequest("You are the last user, You cant delete yourself");
+		} else {
+			User user = User.findById(id);
+			user.delete();
+		}
+		return redirect(routes.AdminController.userAdmin());
 	}
 
 	public static Result user(String email) {
--- app/controllers/EmailController.java
+++ app/controllers/EmailController.java
@@ -3,62 +3,86 @@ package controllers;
 import models.User;
 
 import org.apache.commons.mail.DefaultAuthenticator;
-import org.apache.commons.mail.Email;
+import org.apache.commons.mail.HtmlEmail;
 import org.apache.commons.mail.EmailException;
-import org.apache.commons.mail.SimpleEmail;
 
 import play.Configuration;
 import play.Logger;
 import play.Play;
 import play.mvc.Http.*;
+import views.html.*;
 
 public class EmailController {
 
-    private static Configuration config = Play.application().configuration();
-    private static String hostname = config.getString("smtp.host");
-    private static int port = config.getInt("smtp.port");
-    private static Boolean ssl = config.getBoolean("smtp.ssl");
-    private static Boolean tls = config.getBoolean("smtp.tls");
-    private static String username = config.getString("smtp.user");
-    private static String password = config.getString("smtp.password");
-    private static String fromAddress = config.getString("smtp.from");
+	private static Configuration config = Play.application().configuration();
+	private static String hostname = config.getString("smtp.host");
+	private static int port = config.getInt("smtp.port");
+	private static Boolean ssl = config.getBoolean("smtp.ssl");
+	private static Boolean tls = config.getBoolean("smtp.tls");
+	private static String username = config.getString("smtp.user");
+	private static String password = config.getString("smtp.password");
+	private static String fromAddress = config.getString("smtp.from");
 
+	public static void sendMail(String toAddress, String subject, String msg)
+			throws EmailException {
 
-	public static void sendMail(String toAddress,
-			String subject, String msg) throws EmailException {
-		Email email = new SimpleEmail();
+		Logger.info("Preparing Email");
+
+		HtmlEmail email = new HtmlEmail();
+		email.setHtmlMsg(msg);
+		// Email email = new SimpleEmail();
 		email.setHostName(hostname);
 		email.setSmtpPort(port);
 		email.setAuthenticator(new DefaultAuthenticator(username, password));
 		email.setSSLOnConnect(ssl);
-//      email.setTLS(tls);
+		// email.setTLS(tls);
 		email.setStartTLSEnabled(tls);
 		email.setFrom(fromAddress);
 		email.setSubject(subject);
 		email.setMsg(msg);
 		email.addTo(toAddress);
+
+		Logger.info("Sending Email");
+
 		email.send();
 		Logger.info("EMAIL_SENT: " + fromAddress + " " + toAddress + " "
 				+ subject + " " + msg);
-        Logger.info("Sent email from: "+fromAddress+" To: "+toAddress+" Subject: "+subject+" Message: "+msg);
+		Logger.info("Sent email from: " + fromAddress + " To: " + toAddress
+				+ " Subject: " + subject + " Message: " + msg);
 	}
 
 	public static boolean NotifyNewUser(User newUser, Request request) {
 
 		String subject = "Welcome to MERIT";
-		String msg = "Welcome to "+routes.Application.index().absoluteURL(request)+".<br><br>Your username is "
-				+ newUser.email
-				+ " and your password is "
-				+ newUser.password
-				+ ".<br><br>You may change your password once you log in for the first time.";
+		// String msg = "<h2>Welcome to "
+		// + routes.Application.index().absoluteURL(request)
+		// + ".</h2><br><br>"
+		// + "<p>Your username is "
+		// + newUser.email
+		// + " and your password is "
+		// + newUser.password
+		// +
+		// ".<br>You may change your password once you log in for the first time.</p>";
+
+		String url = routes.Application.index().absoluteURL(request);
+
+		String msg = emailTemplate(newUser.name, newUser.email, url,
+				newUser.password);
 
 		try {
 			sendMail(newUser.email, subject, msg);
-            return true;
+			return true;
 		} catch (EmailException e) {
-            e.printStackTrace();
-            return false;
+			e.printStackTrace();
+			Logger.info("Failed! Sending Email");
+			return false;
 		}
 
+	}
+
+	private static String emailTemplate(String name, String username,
+			String url, String password) {
+		// @(name: String, username: String, url: String, password: String)
+		return newUserEmailTemplate.render(name, username, url, password).toString();
 	}
 }
