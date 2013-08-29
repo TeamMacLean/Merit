4c6f25cd6501e5a984f60244a9a1434d6c1a2afd
Martin Page <wookoouk@gmail.com>
2013-08-29 15:25:48 +0100

    Revert "Added commented out mysql settings"
    This reverts commit 9a49ea1305d11addea1c23df74e329467a04c55b.

--- project/Build.scala
+++ project/Build.scala
@@ -12,11 +12,10 @@ object ApplicationBuild extends Build {
     javaCore,
     javaJdbc,
     javaEbean,
-    "org.webjars" %% "webjars-play" % "2.1.0-2"
-    ,"org.webjars" % "jquery" % "1.10.2"
-    ,"org.webjars" % "flat-ui" % "bcaf2de95e"
-    ,"org.apache.commons" % "commons-email" % "1.3.1"
-    ,"mysql" % "mysql-connector-java" % "5.1.18"
+    "org.webjars" %% "webjars-play" % "2.1.0-2",
+    "org.webjars" % "jquery" % "1.10.2",
+    "org.webjars" % "flat-ui" % "bcaf2de95e",
+    "org.apache.commons" % "commons-email" % "1.3.1"
     )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
