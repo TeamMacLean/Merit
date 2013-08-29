8355d34b16c99e13e1305594a15e0d2f29cadc3c
Martin Page <wookoouk@gmail.com>
2013-08-29 15:26:49 +0100

    test

--- start-80.sh
+++ start-80.sh
@@ -1,1 +1,4 @@
 play "start -Dhttp.port=80 -DapplyEvolutions.default=true"
+
+#New Relic Monitoring
+# play "start -Dhttp.port=80 -DapplyEvolutions.default=true -javaagent:/home/play/newrelic/newrelic.jar -Dnewrelic.bootstrap_classpath=true"
