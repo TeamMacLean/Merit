bc8c9e2509f8f5cc0774248ea8c9c54e92f344f3
Martin Page <wookoouk@gmail.com>
2013-08-29 14:33:22 +0100

    Update start-80.sh

--- start-80.sh
+++ start-80.sh
@@ -1,1 +1,4 @@
-play "start -Dhttp.port=80 -DapplyEvolutions.default=true"
+target/start -Dhttp.port=80 -DapplyEvolutions.default=true
+
+# New Relic support
+# target/start -Dhttp.port=80 -DapplyEvolutions.default=true -javaagent:newrelic/newrelic.jar -Dnewrelic.bootstrap_classpath=true
