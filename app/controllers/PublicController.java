1e4140d3f60332978288cdb465d6ef67a53114c0
Martin Page <wookoouk@gmail.com>
2013-09-09 12:22:43 +0100

    create assertions via gui at /easy

--- app/controllers/PublicController.java
+++ app/controllers/PublicController.java
@@ -8,12 +8,26 @@ import models.BadgeClass;
 import models.Image;
 import models.IssuerOrganization;
 import models.Revocation;
+import play.Logger;
 import play.Play;
+import play.data.Form;
+import play.libs.F.Promise;
 import play.libs.Json;
+import play.libs.WS;
 import play.mvc.Controller;
+import views.html.*;
 import play.mvc.Result;
 
 public class PublicController extends Controller {
+
+	public static class EasyAssertion {
+
+		public String Authorization;
+		public String recipient;
+		public String evidence;
+		public Long badgeId;
+
+	}
 
 	// @BodyParser.Of(play.mvc.BodyParser.Json.class)
 	public static Result getRecocationsAsJson() {
@@ -24,6 +38,41 @@ public class PublicController extends Controller {
 	public static String getAssetUrl(Long id) {
 		return routes.PublicController.getAsset(id).absoluteURL(request());
 
+	}
+
+	public static Result EasyAssertion() {
+		Form<EasyAssertion> assertionForm = new Form<EasyAssertion>(
+				EasyAssertion.class);
+
+		List<BadgeClass> badges = BadgeClass.find.all();
+		
+		return ok(views.html.easyassertion.render(assertionForm, badges));
+//		return TODO;
+	}
+
+	public static Result addAssertion() {
+
+		Form<EasyAssertion> assertionForm = new Form<EasyAssertion>(
+				EasyAssertion.class).bindFromRequest();
+		if (assertionForm.hasErrors()) {
+			return badRequest("ERROR");
+		}
+
+		// form ok
+
+		EasyAssertion ea = assertionForm.get();
+
+		// I wish my wife was this dirty!
+		Promise<WS.Response> result = WS.url(
+				routes.APIController.createBadgeAssertion().absoluteURL(
+						request())).post(
+				"Authorization" + ea.Authorization + "&" + "recipient"
+						+ ea.recipient + "&" + "evidence" + ea.evidence + "&"
+						+ "badgeId=" + ea.badgeId);
+
+		Logger.info("New Assertion response: " + result.get().getStatus());
+
+		return TODO;
 	}
 
 	public static Result getAsset(Long id) {
