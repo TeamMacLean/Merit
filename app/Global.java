b7cf63076a0621af2cd3fa32633f30c3a9f47453
Martin Page <wookoouk@gmail.com>
2013-09-30 14:43:34 +0100

    debuggin

--- app/Global.java
+++ app/Global.java
@@ -1,9 +1,27 @@
+import java.lang.reflect.Method;
+
 import play.*;
+import play.mvc.Action;
+import play.mvc.Http.Request;
 
 import models.*;
 
 public class Global extends GlobalSettings {
 
+	@Override
+	public Action onRequest(Request arg0, Method arg1) {
+		
+		// TODO Auto-generated method stub
+		Logger.info("---Remote address = "+arg0.remoteAddress());
+		Logger.info("---Request URI = "+arg0.uri());
+//		for(String[] s: arg0.headers().values()){
+//			for(String ss: s){
+//				Logger.info("REQUEST HEADER: "+ss);
+//			}
+//		}
+		return super.onRequest(arg0, arg1);
+	}
+	
 	public void onStart(Application app) {
 		Logger.info("Starting Merit");
 		InitialData.insert(app);
