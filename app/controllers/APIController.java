e1361dbef0de6956adc7b0600576366301aacd36
Martin Page <wookoouk@gmail.com>
2013-10-01 14:41:43 +0100

    better response

--- app/controllers/APIController.java
+++ app/controllers/APIController.java
@@ -18,7 +18,7 @@ public class APIController extends Controller {
 
 	private static Configuration config = Play.application().configuration();
 	public static boolean overSSL = config.getBoolean("ssl.routing");
-	
+
 	public static HashMap<String, String> headers;
 
 	public static boolean checkAuth(String apiKey) {
@@ -81,65 +81,60 @@ public class APIController extends Controller {
 		String assertionURL = routes.PublicController.getAssertion(ba.uid)
 				.absoluteURL(request(), overSSL);
 
-
-		
-		
-		
-		
-		
-		
-		if (!EmailController.NotifyNewBadge(recipient,ba, request())) {
+		if (!EmailController.NotifyNewBadge(recipient, ba, request())) {
 			flash(Application.GLOBAL_FLASH_ERROR,
 					"Cound not notify user, please check your email settings");
 		} else {
 			flash(Application.GLOBAL_FLASH_SUCCESS,
 					"Badge created and email sent");
+			if (headers != null) {
+				return redirect(routes.AssertionController.assertions());
+			}
 		}
-		
-		
-		
-//		
-//		ThreadSendEmail emailRunner = new ThreadSendEmail(recipient, evidence,
-//				ba, request());
-//		new Thread(emailRunner).start();
-//
-//		if (headers != null) {
-//			headers = null;
-//			return redirect(routes.AssertionController.assertions());
-//		}
-//		headers = null;
+
+		//
+		// ThreadSendEmail emailRunner = new ThreadSendEmail(recipient,
+		// evidence,
+		// ba, request());
+		// new Thread(emailRunner).start();
+		//
+		// if (headers != null) {
+		// headers = null;
+		// return redirect(routes.AssertionController.assertions());
+		// }
+		// headers = null;
 		return ok(assertionURL);
 
 		// return badRequest("Could not create assertion");
 	}
 
-//	private static class ThreadSendEmail implements Runnable {
-//		String recipient;
-//		String evidence;
-//		BadgeAssertion ba;
-//		Request request;
-//
-//		public ThreadSendEmail(String recipient, String evidence,
-//				BadgeAssertion ba, Request request) {
-//			// TODO Auto-generated constructor stub
-//			this.recipient = recipient;
-//			this.evidence = evidence;
-//			this.ba = ba;
-//			this.request = request;
-//		}
-//
-//		@Override
-//		public void run() {
-//			try {
-//				String url = routes.PublicController.giveBadge(ba.uid)
-//						.absoluteURL(request, overSSL);
-//				EmailController.sendMail(recipient, "You earnt a badge!",
-//						"You have received a badge to show your work on "
-//								+ evidence + ". " + url);
-//				;
-//			} catch (EmailException e) {
-//				e.printStackTrace();
-//			}
-//		}
-//	}
+	// private static class ThreadSendEmail implements Runnable {
+	// String recipient;
+	// String evidence;
+	// BadgeAssertion ba;
+	// Request request;
+	//
+	// public ThreadSendEmail(String recipient, String evidence,
+	// BadgeAssertion ba, Request request) {
+	// // TODO Auto-generated constructor stub
+	// this.recipient = recipient;
+	// this.evidence = evidence;
+	// this.ba = ba;
+	// this.request = request;
+	// }
+	//
+	// @Override
+	// public void run() {
+	// try {
+	// String url = routes.PublicController.giveBadge(ba.uid)
+	// .absoluteURL(request, overSSL);
+	// EmailController.sendMail(recipient, "You earnt a badge!",
+	// "You have received a badge to show your work on "
+	// + evidence + ". " + url);
+	// ;
+	// } catch (EmailException e) {
+	// e.printStackTrace();
+	// }
+	// }
+	// }
 }
