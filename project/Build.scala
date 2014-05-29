44da6cdcdb7cd737d1902a31e5b4ff7d7db33ebf
Martin Page <wookoouk@gmail.com>
2014-05-29 14:24:52 +0100

    Update Build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -12,15 +12,17 @@ object ApplicationBuild extends Build {
     javaCore,
     javaJdbc,
     javaEbean,
-    "org.webjars" %% "webjars-play" % "2.1.0-2",
+    "org.webjars" %% "webjars-play" % "2.2.1-2",
     "org.webjars" % "jquery" % "1.10.2",
     "org.webjars" % "flat-ui" % "bcaf2de95e",
     "org.apache.commons" % "commons-email" % "1.3.1",
-    "mysql" % "mysql-connector-java" % "5.1.18"
+    "mysql" % "mysql-connector-java" % "5.1.18",
+    "org.codehaus.jackson" % "jackson-core-lgpl" % "1.9.13",
+    "org.codehaus.jackson" % "jackson-mapper-lgpl" % "1.9.13"
     )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here  
+    // Add your own project settings here
   )
 
 }
